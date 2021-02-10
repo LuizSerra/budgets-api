8ead37fbbece1f5fcc4eb83eaa4d038d5111e542
Luiz Serra <lserra.trade@gmail.com>
2021-02-10 19:50:35 -0300

    Estrutura

---
+++ budgets-api/src/main/java/com/netmax/budget/BudgetsApiApplication.java
@@ -0,0 +1,13 @@
+package com.netmax.budget;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BudgetsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BudgetsApiApplication.class, args);
+	}
+
+}
