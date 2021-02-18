67e24288befc06bb31535abc90069f570cf568ea
Luiz Serra <lserra.trade@gmail.com>
2021-02-17 22:37:28 -0300

    Iniciando tratamento de erros com ResponseEntityExceptionHandler

--- src/main/java/com/netmaxi/budget/controller/ServicoController.java
+++ src/main/java/com/netmaxi/budget/controller/ServicoController.java
@@ -7,14 +7,12 @@ import java.util.Optional;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
