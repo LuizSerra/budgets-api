e3345d10610e2ce62d92474243110caed855ffde
Luiz Serra <lserra.trade@gmail.com>
2021-08-23 09:40:53 -0300

    Documentação com Swagger

--- src/main/java/com/netmaxi/budget/BudgetApiApplication.java
+++ src/main/java/com/netmaxi/budget/BudgetApiApplication.java
@@ -3,7 +3,10 @@ package com.netmaxi.budget;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @SpringBootApplication
+@EnableSwagger2
 public class BudgetApiApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/netmaxi/budget/config/security/SecurityConfigurations.java
+++ src/main/java/com/netmaxi/budget/config/security/SecurityConfigurations.java
@@ -54,6 +54,9 @@ public class SecurityConfigurations extends WebSecurityConfigurerAdapter {
 	}
 	
 	@Override
-	public void configure(WebSecurity web) throws Exception {	}
+	public void configure(WebSecurity web) throws Exception {
+	    web.ignoring()
+	        .antMatchers("/**.html", "/v2/api-docs", "/webjars/**", "/configuration/**", "/swagger-resources/**");
+	}
 
 }
